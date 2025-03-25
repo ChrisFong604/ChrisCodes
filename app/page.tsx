bf2607126823fa5664e53f8c55fc76c67db03945
ChrisFong604 <ccffoonngg@gmail.com>
2025-03-25 16:00:39 -0700

    updated to support pictures and videos

--- app/page.tsx
+++ app/page.tsx
@@ -39,11 +39,12 @@ const TRANSITION_SECTION = {
   duration: 0.3,
 };
 
-type ProjectVideoProps = {
-  src: string;
+type ProjectMediaProps = {
+  mediaType: "video" | "image";
+  mediaUrl: string;
 };
 
-function ProjectVideo({ src }: ProjectVideoProps) {
+function ProjectMedia({ mediaType, mediaUrl }: ProjectMediaProps) {
   return (
     <MorphingDialog
       transition={{
@@ -53,23 +54,46 @@ function ProjectVideo({ src }: ProjectVideoProps) {
       }}
     >
       <MorphingDialogTrigger>
-        <video
-          src={src}
+        {mediaType === "video" ? (
+          <video
+            src={mediaUrl}
+            autoPlay
+            loop
+            muted
+            className="aspect-video w-full cursor-zoom-in rounded-xl"
+          />
+        ) : (
+          <img
+            src={mediaUrl}
+            alt="project image"
+            className="aspect-video w-full cursor-zoom-in rounded-xl"
+          />
+        )}
+        {/* <video
+          src={mediaUrl}
           autoPlay
           loop
           muted
           className="aspect-video w-full cursor-zoom-in rounded-xl"
-        />
+        /> */}
       </MorphingDialogTrigger>
       <MorphingDialogContainer>
         <MorphingDialogContent className="relative aspect-video rounded-2xl bg-zinc-50 p-1 ring-1 ring-zinc-200/50 ring-inset dark:bg-zinc-950 dark:ring-zinc-800/50">
-          <video
-            src={src}
-            autoPlay
-            loop
-            muted
-            className="aspect-video h-[50vh] w-full rounded-xl md:h-[70vh]"
-          />
+          {mediaType === "video" ? (
+            <video
+              src={mediaUrl}
+              autoPlay
+              loop
+              muted
+              className="aspect-video h-[50vh] w-full rounded-xl md:h-[70vh]"
+            />
+          ) : (
+            <img
+              src={mediaUrl}
+              alt="project image"
+              className="aspect-video h-[50vh] w-full rounded-xl md:h-[70vh]"
+            />
+          )}
         </MorphingDialogContent>
         <MorphingDialogClose
           className="fixed top-6 right-6 h-fit w-fit rounded-full bg-white p-1"
@@ -150,12 +174,15 @@ export default function Personal() {
         variants={VARIANTS_SECTION}
         transition={TRANSITION_SECTION}
       >
-        <h3 className="mb-5 text-lg font-medium">Selected Projects</h3>
+        <h3 className="mb-5 text-lg font-medium">Projects</h3>
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
           {PROJECTS.map((project) => (
             <div key={project.name} className="space-y-2">
               <div className="relative rounded-2xl bg-zinc-50/40 p-1 ring-1 ring-zinc-200/50 ring-inset dark:bg-zinc-950/40 dark:ring-zinc-800/50">
-                <ProjectVideo src={project.video} />
+                <ProjectMedia
+                  mediaUrl={project.mediaUrl}
+                  mediaType={project.mediaType}
+                />
               </div>
               <div className="px-1">
                 <a
@@ -164,7 +191,7 @@ export default function Personal() {
                   target="_blank"
                 >
                   {project.name}
-                  <span className="absolute bottom-0.5 left-0 block h-[1px] w-full max-w-0 bg-zinc-900 transition-all duration-200 group-hover:max-w-full"></span>
+                  <span className="absolute bottom-0.5 left-0 block h-[1px] w-full max-w-0 bg-zinc-900 transition-all duration-500 group-hover:max-w-full"></span>
                 </a>
                 <p className="text-base text-zinc-600 dark:text-zinc-400">
                   {project.description}
